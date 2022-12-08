a0776e1b4968cd2f55edfb6566f15e62ca02309f
victory <victoryilikah58@gmail.com>
2022-12-08 04:38:19 -0800

    added new file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * main - main function of projest
+ *
+ * Return: return 0 everyhtime
+ */
+int main(void)
+{
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return(0);
+}
