5c2cec3be65821ba48e420407de3db74a93e1e49
victory <victoryilikah58@gmail.com>
2022-12-09 15:37:24 -0800

    Added new files

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,26 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there */
-/**
- *  * main- this is the entry point of the program
- *   *
- *    * Return: this returns a default value of 0
- *    */
-int main(void)
-{
-	int n;
-	int ld;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	ld = n % 10;
-	if (ld > 5)
-	{
-			printf("Last digit of %d is %d and is greater than 5\n", n, ld);
-		}
-
-	if (ld == 0)
-	{
-			printf("Last digit of %d is %d and is 0\n", n, ld);
-		}
-
-	if (ld < 6 && ld != 0)
-	{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-		}
-
-	return (0);
-}
+#include <stdio.h> 
+ #include <stdlib.h> 
+ #include <time.h> 
+ /** 
+  *  *main - entry point of programs 
+  *   * 
+  *    *Return: return 0 
+  *     */ 
+ int main(void) 
+	 { 
+	          int n; 
+	   
+	          srand(time(0)); 
+	          n = rand() - RAND_MAX / 2; 
+	          if (n < 0) 
+	          { 
+	                   printf("%d is negative\n", n); 
+	           } 
+	          else if (n > 0) 
+	          { 
+	                   printf("%d is positive\n", n); 
+	           } 
+	          else 
+	                  printf("%d is zero\n", n); 
+	          return (0); 
+	  }}
