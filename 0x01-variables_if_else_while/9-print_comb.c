8f8ffdc41fb8a8bea89652ca14b6afe53b5ae736
victory <victoryilikah58@gmail.com>
2022-12-12 04:05:57 -0800

    Added new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main- this is the entry point of the program
+ *
+ * Return: this returns a default value of 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
