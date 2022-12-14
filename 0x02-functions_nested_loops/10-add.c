42912282be83072c4d6858f739131257413d42d5
victory <victoryilikah58@gmail.com>
2022-12-14 10:50:00 -0800

    added new files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - this is the entry point of the program
+ * @a: the first parameter
+ * @b: second parameter
+ * Description: this function returns 1 or 0
+ * Return: 1 success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
