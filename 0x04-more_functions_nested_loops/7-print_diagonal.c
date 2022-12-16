3cc43b909d6896e9afa7fddf746f698b5a848d12
victory <victoryilikah58@gmail.com>
2022-12-16 08:14:49 -0800

    added new files

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal based on user input
+ * @n: user input
+ */
+
+void print_diagonal(int n)
+{
+	int iter;
+	int space;
+
+	if (n <= 0)
+	{
+		_putchar(10);
+
+		return;
+	}
+
+	for (iter = 0; iter < n; iter++)
+	{
+		space = 0;
+
+		while (space < iter)
+		{
+			_putchar(' ');
+			space++;
+		}
+
+		_putchar('\\');
+		_putchar(10);
+	}
+}
